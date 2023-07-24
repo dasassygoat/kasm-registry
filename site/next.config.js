894eb54a7b9a22f9583b4a70e1e996fdd75d9096
Bryan Pritchett <bryan@coderbox.co>
2023-07-24 02:00:56 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Lostlooper Technologies',
+    description: 'Lostlooper Technologies for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://journeyman32.github.io/kasm-registry',
+    contactUrl: 'https://github.com/journeyman32/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
